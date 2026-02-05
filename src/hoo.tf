2ca70e5e0a083cbb18c6429d8e228723cef32afd
Bhekani Mdletshe <bhekani.mdletsher@gmail.com>
2026-02-05 21:42:23 +0000

    Update file hoo.tf

--- src/hoo.tf
+++ src/hoo.tf
@@ -1,5 +1,5 @@
 resource "aws_connect_hours_of_operation" "EDB_HOO" {
-  instance_id = "aaaaaaaa-bbbb-cccc-dddd-111111111111"
+  instance_id = var.instance_id
   name        = "Office Hours"
   description = "Monday office hours"
   time_zone   = "EST"
