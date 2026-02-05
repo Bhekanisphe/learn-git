7056544a914d2a8f9f6d6dae8de1b3d20d65ccdb
Bhekani Mdletshe <bhekani.mdletsher@gmail.com>
2026-02-05 21:55:58 +0000

    Update file queues.tf

--- src/queues.tf
+++ src/queues.tf
@@ -2,7 +2,7 @@ resource "aws_connect_queue" "EDB_CCS_Debit" {
   instance_id           = var.instance_id
   name                  = "EDB CCS Debit"
   description           = "EDB CCS Debit Queue"
-  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.id
+  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.hours_of_operation_id
 
   tags = {
     "Name" = "Example Queue",
