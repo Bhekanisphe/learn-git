4cb941bfb02efe995fbe06e4e876387164354cc2
Bhekani Mdletshe <bhekani.mdletsher@gmail.com>
2026-02-05 22:00:26 +0000

    Update file queues.tf

--- src/queues.tf
+++ src/queues.tf
@@ -8,3 +8,14 @@ resource "aws_connect_queue" "EDB_CCS_Debit" {
     "Name" = "Example Queue",
   }
 }
+
+resource "aws_connect_queue" "EDB_CCS_Credit" {
+  instance_id           = var.instance_id
+  name                  = "EDB CCS Credit"
+  description           = "EDB CCS Credit Queue"
+  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.hours_of_operation_id
+
+  tags = {
+    "Name" = "Example Queue - 2",
+  }
+}
