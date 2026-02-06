ac46143e6b26e7e3ab256841f52c197ed97fc1ae
Bhekani Mdletshe <bhekani.mdletsher@gmail.com>
2026-02-06 05:49:05 +0000

    B new

---
+++ src/queues.tf
@@ -0,0 +1,21 @@
+resource "aws_connect_queue" "EDB_CCS_Debit" {
+  instance_id           = var.instance_id
+  name                  = "EDB CCS Debit"
+  description           = "EDB CCS Debit Queue"
+  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.hours_of_operation_id
+
+  tags = {
+    "Name" = "Example Queue",
+  }
+}
+
+resource "aws_connect_queue" "EDB_CCS_Credit" {
+  instance_id           = var.instance_id
+  name                  = "EDB CCS Credit"
+  description           = "EDB CCS Credit Queue"
+  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.hours_of_operation_id
+
+  tags = {
+    "Name" = "Example Queue - 2",
+  }
+}
