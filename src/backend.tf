413a827cd1f306fe0ddfa584bbe1109cf62c5d2e
Bhekani Mdletshe <bhekani.mdletsher@gmail.com>
2026-02-06 07:47:22 +0000

    Update file backend.tf

--- src/backend.tf
+++ src/backend.tf
@@ -2,5 +2,6 @@ terraform{
     backend "http"{
         address = "https://gitlab.com/api/v4/projects/78204586/terraform/state/$TF_STATE_NAME"
         username = "bhekani.mdletsher"
+        TF_HTTP_PASSWORD = $TOKEN
     }
 }
