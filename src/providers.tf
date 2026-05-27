dabb2fee17660fe17d4e30a319dfbf3c4196c97c
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-05-27 23:13:29 +0200

    fix Jenkinfile

--- src/providers.tf
+++ src/providers.tf
@@ -4,31 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-
-    gitlab = {
-      source = "gitlabhq/gitlab"
-      version = "18.8.2"
-    }
-
   }
 }
 
 provider "aws" {
   region = "af-south-1"
-}
-
-variable "TOKEN" {
-  type = string
-  sensitive = true
-}
-
-provider "gitlab" {
-  base_url = "https://https://gitlab.com/bhekani.mdletsher-group/api/v4/"
-  token = var.TOKEN
 }
 
