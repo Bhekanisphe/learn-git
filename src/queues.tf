460bdbb5924f256f16df5d6be5fcea9a51115cff
Bhekanisphe <166112842+Bhekanisphe@users.noreply.github.com>
2026-06-02 11:15:59 +0200

    Refactor AWS Connect queue resources
    Removed duplicate resource declarations for queues and cleaned up the code.

--- src/queues.tf
+++ src/queues.tf
@@ -20,16 +20,6 @@ resource "aws_connect_queue" "EDB_CCS_Credit" {
   }
 }
 
-resource "aws_connect_queue" "EDB_CCS_Debit" {
-  instance_id           = var.instance_id
-  name                  = "EDB CCS Debit - IaC"
-  description           = "EDB CCS Debit Queue"
-  hours_of_operation_id = aws_connect_hours_of_operation.EDB_HOO.hours_of_operation_id
-
-  tags = {
-    "Name" = "Example Queue",
-  }
-}
 
 resource "aws_connect_queue" "Technical_Wifi" {
   instance_id           = var.instance_id
