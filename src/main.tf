6389ffd7c2c5f51af906436f081f8f08d047dfa4
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-08 13:47:29 +0200

    added import apply add stage fix-- added import

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,4 @@
 import {
   to = aws_connect_contact_flow.BS-Test-Flow
-  id = "${var.instance_id}:24285ce2-fdf0-4c86-8c1c-380b19d747af"
+  id = "${var.instance_id}:f4568b43-f150-481a-9712-76aa79052771"
 }
