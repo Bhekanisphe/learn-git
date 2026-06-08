c268ca1b3d53c6ccb83b8bcc53390f9f3ffbf236
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-08 13:51:47 +0200

    added import apply add stage fix-- added import

--- src/flows.tf
+++ src/flows.tf
@@ -1,7 +1,7 @@
 resource "aws_connect_contact_flow" "BS-Test-Flow" {
   instance_id  = var.instance_id
-  name         = "BS-Test-Flow - IaC"
-  description  = "Test Contact Flow IaC"
+  name         = "BS-Test-Flow"
+  description  = "Test Contact Flow created by IaC"
   type         = "CONTACT_FLOW"
   tags = {
     "Name"        = "Test Contact Flow IaC",
