1fd57b0daea345cae78f48199a41958cb72075d5
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-08 10:18:53 +0200

    added flow as IaC fix

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,4 @@
 import {
   to = aws_connect_contact_flow.BS-Test-Flow
-  id = "var.instance_id:c97e5f73-bf06-4237-b0e7-ee5e4a20946e"
+  id = "${var.instance_id}:c97e5f73-bf06-4237-b0e7-ee5e4a20946e"
 }
