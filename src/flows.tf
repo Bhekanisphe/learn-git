d4d80893d4e54def04638be1d86e371e14f94657
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-09 13:25:06 +0200

    added import apply add stage fix-- added import

--- src/flows.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_connect_contact_flow" "BS-Test-Flow" {
-  instance_id  = var.instance_id
-  name         = "BS-Test-Flow"
-  description  = "Test Contact Flow created by IaC"
-  type         = "CONTACT_FLOW"
-  tags = {
-    "Name"        = "Test Contact Flow IaC",
-  }
-}
