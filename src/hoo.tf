d730aafb4ab4a68fb7094f1a44eb9c7dd69b4f56
bhekani.mdletsher <bhekani.mdletsher@gmail.com>
2026-06-17 11:02:45 +0200

    Tidy up - fix 3

--- src/hoo.tf
+++ src/hoo.tf
@@ -4,16 +4,18 @@ resource "aws_connect_hours_of_operation" "BS-Test-HOO" {
   description = "Bs test flow office hours of operation"
   time_zone   = "EST"
 
-  config {
+  dynamic "config" {
     for_each = var.days_of_week
-    day = each.value
-    start_time {
-      hours   = 9
-      minutes = 0
-    }
-    end_time {
-      hours   = 17
-      minutes = 0
+    content {
+      day = config.value
+      start_time {
+        hours   = 9
+        minutes = 0
+      }
+      end_time {
+        hours   = 17
+        minutes = 0
+      }
     }
   }
 
